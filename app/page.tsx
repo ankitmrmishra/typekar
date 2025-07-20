55d5260dc7eadc4242ac6cc7600334d0e77f05a0
ankitmrmishra <ankitmrmishra1118@gmail.com>
2025-07-21 01:17:39 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,131 @@
+"use client";
 import Image from "next/image";
+import texts from "@/lib/texts.json";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [paragraph, setParagraph] = useState<string>();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [userInput, setUserInput] = useState("");
+  const [charIndex, setCharIndex] = useState(0);
+
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(50);
+
+  const randomIndex = Math.floor(Math.random() * texts.length);
+  const radnom_paragraph = texts[randomIndex];
+  const initial_para = radnom_paragraph
+    .split(" ")
+    .slice(start, end)
+    .join(" ")
+    .toLowerCase();
+  useEffect(() => {
+    setParagraph(initial_para);
+  }, [end]);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const { key } = event;
+
+      if (key === " " || key === "Backspace") {
+        event.preventDefault();
+      }
+
+      if (key.length > 1 && key !== "Backspace") return;
+      console.log("A key was pressed:", event.key);
+
+      if (key === "Backspace") {
+        setUserInput((prevInput) => prevInput.slice(0, -1));
+        setCharIndex((prevIndex) => Math.max(0, prevIndex - 1));
+      } else {
+        setUserInput((prevInput) => prevInput + key);
+        setCharIndex((prevIndex) => prevIndex + 1);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [end]);
+
+  let wordcounts = userInput.split(" ").filter((word) => word !== "");
+
+  const actualPara = paragraph?.split(" ").slice(start, end) || [];
+  const userpara = userInput.split(" ");
+
+  const correctWords = userpara.filter((word, index) => {
+    return word.length > 0 && word === actualPara[index];
+  });
+
+  return (
+    <div className="bg-black text-white w-screen h-screen flex justify-around items-center align-middle flex-col  ">
+      <div className="flex justify-center align-middle items-center gap-2">
+        words:
+        <div
+          className=" bg-white/20 p-1 rounded-sm hover:cursor-pointer hover:text-orange-500"
+          onClick={() => {
+            setEnd(10);
+            setCharIndex(0);
+            setUserInput("");
+          }}
+        >
+          10
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          className=" bg-white/20 p-1 rounded-sm hover:cursor-pointer hover:text-orange-500"
+          onClick={() => {
+            setEnd(30);
+            setCharIndex(0);
+            setUserInput("");
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          30
+        </div>
+        <div
+          className=" bg-white/20 p-1 rounded-sm hover:cursor-pointer hover:text-orange-500"
+          onClick={() => {
+            setEnd(70);
+            setCharIndex(0);
+            setUserInput("");
+          }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          70
+        </div>
+      </div>
+      <div className="flex flex-col text-start justify-center align-middle items-center">
+        <div className="text-3xl">
+          {wordcounts.length}/{end}
+          <div className="">Correct words : {correctWords.length}</div>
+        </div>
+        <div className={cn("text-xl tracking-widest max-w-5xl leading-[3rem]")}>
+          {paragraph?.split("").map((char, i) => {
+            let charState = "untyped";
+
+            if (i < charIndex) {
+              charState = char === userInput[i] ? "correct" : "incorrect";
+            } else if (i === charIndex) {
+              charState = "current";
+            }
+
+            return (
+              <span
+                key={i}
+                className={cn({
+                  "text-gray-700": charState === "untyped",
+                  "text-green-500": charState === "correct",
+                  "text-red-500": charState === "incorrect",
+                  "text-white": charState === "current",
+                })}
+              >
+                {char}
+              </span>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
